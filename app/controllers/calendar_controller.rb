c6ee898c1f64313fb805413611bc48bb6f678429
hasyrails <hasyrails@gmail.com>
2020-09-03 21:12:06 +0900

    #13 / CalendarController実装

---
+++ app/controllers/calendar_controller.rb
@@ -0,0 +1,32 @@
+class CalendarController < ApplicationController
+  before_action :set_calendar, only: [:delete]
+
+  def index
+    @calendar = Calendar.all
+  end
+
+  def new
+    @calendar = Calendar.new
+  end
+
+  def create
+    @calendar = Calendar.create(calendar_params)
+
+    if @calendar.save
+      redirect_to calendar_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def calendar_params
+    params.require(:calendar).permit(:name, :start, :end)
+  end
+
+  def set_calendar
+    @calendar = Calendar.find(params[:id])
+  end
+
+end
