4856a5094ec5111440ab25d719991376a9036a34
hasyrails <hasyrails@gmail.com>
2020-09-03 20:31:29 +0900

    #11 / 予定削除を確認完了(deleteEventメソッド)

--- app/javascript/components/CalendarComponent.vue
+++ app/javascript/components/CalendarComponent.vue
@@ -42,6 +42,40 @@
           @click:day="createEvent"
       ></v-calendar>
     </v-sheet>
+
+    <v-menu
+        v-model="eventAlert"
+        :close-on-content-click="false"
+        :activator="selectedItem"
+      >
+        <v-card
+          color="grey lighten-4"
+          min-width="350px"
+          flat
+        >
+
+        <v-card-text>
+          <span v-html="selectedEvent.id"></span>
+          <span v-html="selectedEvent.name"></span>
+        </v-card-text>
+
+        <v-btn
+          text
+          color="primary"
+          @click="eventAlert = false"
+        >
+          閉じる
+        </v-btn>
+
+        <v-btn
+          text
+          color="error"
+          @click="deleteEvent"
+        >
+          削除
+        </v-btn>
+      </v-card>
+    </v-menu>
   </div>
 </template>
 
@@ -56,16 +90,21 @@
     datas:[],
     events: [
         {
+          id: 1,
           name: 'あたりまえ体操をする',
           start: '2020-05-18',
           end: '2020-05-1',
         },
         {
+          id: 2,
           name: 'Meeting',
           start: '2020-05-1 09:00',
           end: '2020-05-1 10:00',
         },
       ],
+      eventAlert:false,
+      selectedItem : null,
+      selectedEvent : {},
     }),
     components: {
       createComponent
@@ -90,13 +129,21 @@
       },
        // event → day の順番で呼ばれる。
       clickEvent( {nativeEvent,event} ){
-        console.log("clickEvetn");
-        console.log(event);
-        console.log(event.name)
-        
+        this.selectedEvent = event;
+        this.selectedItem = nativeEvent.target;
+        //よくわからんが時間差でやるとうまく動作する。
+        setTimeout(() => this.eventAlert = true, 10);
         //次にcreateEventが走ってしまうのを防御する
         nativeEvent.stopPropagation();
-        
+      },
+      deleteEvent(){
+        // 正しい削除処理はLaravelがまだできない
+        // とりあえずnewArrayにfilterした結果を突っ込んで返すだけ。
+        let newArray = this.events.filter( item => {
+          return item.id != this.selectedEvent.id
+      });
+        this.events = newArray;
+        this.eventAlert = false;
       }
     }
   }
